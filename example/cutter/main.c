5a803c968e69d10f77dc4e0d20c16eb63bee1ae3
wwhai <cnwwhai@gmail.com>
2023-04-10 00:12:00 +0800

    dev: add simple data

---
+++ example/cutter/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void w(int type)
+{
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    FILE *fp = NULL;
+    fp = fopen("simple.csv", "w+");
+    // V : A -> 25.00 -25.99 全新刀
+    // V : B -> 22-25        良好刀
+    // V : C -> 20-22        磨损严重需要维护刀
+    // V : D -> <20          报废刀
+    double down = 00.00, top = 20.00;
+    for (int i = 0; i < 1000; i++)
+    {
+        double d1 = down + ((double)rand() / RAND_MAX) * (top - down);
+        double d2 = down + ((double)rand() / RAND_MAX) * (top - down);
+        double d3 = down + ((double)rand() / RAND_MAX) * (top - down);
+        double d4 = down + ((double)rand() / RAND_MAX) * (top - down);
+        fprintf(fp, "%2.2f,%2.2f,%2.2f,%2.2f,D\n", d1, d2, d3, d4);
+    }
+    //----------------------------------------------------------------------------------------------
+    {
+        double down = 00.00, top = 20.00;
+        for (int i = 0; i < 1000; i++)
+        {
+            double d1 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d2 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d3 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d4 = down + ((double)rand() / RAND_MAX) * (top - down);
+            fprintf(fp, "%2.2f,%2.2f,%2.2f,%2.2f,D\n", d1, d2, d3, d4);
+        }
+    }
+    {
+        double down = 20.00, top = 22.00;
+        for (int i = 0; i < 1000; i++)
+        {
+            double d1 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d2 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d3 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d4 = down + ((double)rand() / RAND_MAX) * (top - down);
+            fprintf(fp, "%2.2f,%2.2f,%2.2f,%2.2f,C\n", d1, d2, d3, d4);
+        }
+    }
+    {
+        double down = 22.00, top = 25.00;
+        for (int i = 0; i < 1000; i++)
+        {
+            double d1 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d2 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d3 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d4 = down + ((double)rand() / RAND_MAX) * (top - down);
+            fprintf(fp, "%2.2f,%2.2f,%2.2f,%2.2f,B\n", d1, d2, d3, d4);
+        }
+    }
+    {
+        double down = 25.00, top = 25.99;
+        for (int i = 0; i < 1000; i++)
+        {
+            double d1 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d2 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d3 = down + ((double)rand() / RAND_MAX) * (top - down);
+            double d4 = down + ((double)rand() / RAND_MAX) * (top - down);
+            fprintf(fp, "%2.2f,%2.2f,%2.2f,%2.2f,A\n", d1, d2, d3, d4);
+        }
+    }
+
+    fclose(fp);
+}
